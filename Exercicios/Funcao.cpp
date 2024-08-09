1746c7cf14d49570edcebdf7a8fbe1831f343e82
Davi-Pedrosa <daviespinosa037@gmail.com>
2024-08-08 22:10:46 -0300

    ok

--- Exercicios/Funcao.cpp
+++ Exercicios/Funcao.cpp
@@ -3,9 +3,6 @@
 
 
 int media(int n1, int n2){
-	
-	
-	
 	int media = (n1 + n2)/2;
 	return media;
 }
---
+++ Exercicios/LacoRepeticao.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	 
+    float numeros[15]; // Array para armazenar os 15 números
+    int i;
+
+    // Laço para ler os 15 números
+    for(i = 0; i < 15; i++) {
+        printf("Digite o número %d: ", i+1);
+        scanf("%f", &numeros[i]);
+    }
+    
+    
+    
+    
+}
